6cedec846c5a89ca0a91519f5ffd1fd9a772dbc9
Santiago Pulido Diaz <santiagopulido12@gmail.com>
2024-06-04 22:17:33 +0200

    new project added

--- scripts/render.js
+++ scripts/render.js
@@ -14,25 +14,60 @@ function render() {
   });
 
   const projects = getCurrentProjects();
+  
   const firstProject = projects[0];
   if (!firstProject) {
     return;
   }
 
-  const array = [];
+  const arrayFirst = [];
   if(firstProject.description){
-    console.log(firstProject.description);
    
     var arraytho = firstProject.description.split(',');
-    array.push(arraytho);
-    console.log(array);
+    arrayFirst.push(arraytho);
   }
 
-  let opciones = "";
+  let opciones1 = "";
 
-  if(array.length){
-    array[0].forEach((p) => {
-      opciones+=`<button class="btn-common">${p}</button>` ;
+  if(arrayFirst.length){
+    arrayFirst[0].forEach((p) => {
+      opciones1+=`<button class="btn-common">${p}</button>` ;
+    });
+  }
+
+  const secondProject = projects[1];
+  if (!secondProject ) return;
+
+  const arraySecond = [];
+  if(secondProject.description){
+   
+    var arraytho = secondProject.description.split(',');
+    arraySecond.push(arraytho);
+  }
+
+  let opciones2 = "";
+
+  if(arraySecond.length){
+    arraySecond[0].forEach((p) => {
+      opciones2+=`<button class="btn-common">${p}</button>` ;
+    });
+  }
+
+  const thirdProject = projects[2];
+  if (!thirdProject ) return;
+
+  const arrayThird = [];
+  if(secondProject.description){
+   
+    var arraytho = thirdProject.description.split(',');
+    arrayThird.push(arraytho);
+  }
+
+  let opciones3 = "";
+
+  if(arrayThird.length){
+    arrayThird[0].forEach((p) => {
+      opciones3+=`<button class="btn-common">${p}</button>` ;
     });
   }
   gallery.innerHTML = `
@@ -44,10 +79,29 @@ function render() {
           </a>
           <div class="section-project">
             <h3>${firstProject.name}</h3>
-            ${opciones}
+            ${opciones1}
             </div>
       </div>
-      <div class="half-width" id="projects-container"></div>
+      <div class="half-width" id="projects-container">
+      <div class="full-height d-flex-p-b" style="background-image: url('https://images.unsplash.com/photo-1658328565500-c5c668e6a52b?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); background-position: center;">
+      <a class="btona" href="https://car-showcase-mwqlc845s-santidev12.vercel.app/" target="blank_">
+          <i class="fa-solid fa-arrow-up-right-from-square"></i>
+        </a>
+        <div class="section-project">
+          <h3>${projects[1].name} </h3>
+          ${opciones2}
+        </div>
+      </div>
+      <div class="full-height d-flex-p-b" style="background-image: url('https://images.unsplash.com/photo-1578242174372-e26b3681587f?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); background-position: center;">
+      <a class="btona" href="https://calendariomern-production.up.railway.app/" target="blank_">
+          <i class="fa-solid fa-arrow-up-right-from-square"></i>
+        </a>
+        <div class="section-project">
+          <h3>${thirdProject.name} </h3>
+          ${opciones3}
+        </div>
+      </div>
+      </div>
   `;
 
   const projectsDiv = document.getElementById("projects-container");
@@ -62,18 +116,18 @@ function render() {
           descripciones+=`<button class="btn-common">${p}</button>` ;
         });
       }
-      projectsDiv.innerHTML += `
-        <div class="full-height d-flex-p-b" style="background-image: 
-        url('https://images.unsplash.com/photo-1486326658981-ed68abe5868e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80'); background-position: center;">
-        <a class="btona" href="https://car-showcase-mwqlc845s-santidev12.vercel.app/" target="blank_">
-            <i class="fa-solid fa-arrow-up-right-from-square"></i>
-          </a>
-          <div class="section-project">
-            <h3>${project.name} </h3>
-            ${descripciones}
-          </div>
-        </div>
-      `;
+      // projectsDiv.innerHTML += `
+      //   <div class="full-height d-flex-p-b" style="background-image: 
+      //   url("${project.img}"); background-position: center;">
+      //   <a class="btona" href="https://car-showcase-mwqlc845s-santidev12.vercel.app/" target="blank_">
+      //       <i class="fa-solid fa-arrow-up-right-from-square"></i>
+      //     </a>
+      //     <div class="section-project">
+      //       <h3>${project.name} </h3>
+      //       ${descripciones}
+      //     </div>
+      //   </div>
+      // `;
     }
   });
 }
